547bf7a1979ffc4684b1879ec881826fa5effcad
Adam Fazli <120200477+AdamFazli@users.noreply.github.com>
2025-07-01 23:14:54 +0800

    add /

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,6 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
+        <base href="{{ url('/') }}">
         @stack('scripts')
 
         <title>{{ config('app.name', 'Laravel') }}</title>
