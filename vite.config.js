d55f0f60426f362200d7b32b43e1d8328e6f9f13
Adam Fazli <120200477+AdamFazli@users.noreply.github.com>
2025-07-02 00:19:25 +0800

    Updated config in vite

--- vite.config.js
+++ vite.config.js
@@ -2,14 +2,14 @@ import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
-    base: '/', // Ensure base path is set to the root
-    plugins: [
-        laravel({
-            input: [
-                'resources/css/app.css',
-                'resources/js/app.js',
-            ],
-            refresh: true,
-        }),
-    ],
+  base: '/myapp/',  // Set base to the subdirectory
+  plugins: [
+    laravel({
+      input: [
+        'resources/css/app.css',
+        'resources/js/app.js',
+      ],
+      refresh: true,
+    }),
+  ],
 });
