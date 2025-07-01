454d1e8a39a6c8e75c8ee07583bdafe7e5d7fa6c
Adam Fazli <120200477+AdamFazli@users.noreply.github.com>
2025-07-02 00:34:02 +0800

    back to default

--- vite.config.js
+++ vite.config.js
@@ -2,14 +2,13 @@ import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
-  base: '/myapp/',  // Set base to the subdirectory
-  plugins: [
-    laravel({
-      input: [
-        'resources/css/app.css',
-        'resources/js/app.js',
-      ],
-      refresh: true,
-    }),
-  ],
+    plugins: [
+        laravel({
+            input: [
+                'resources/css/app.css',
+                'resources/js/app.js',
+            ],
+            refresh: true,
+        }),
+    ],
 });
