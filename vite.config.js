ea67462a96988f363eeea2c120723791c80fac08
Adam Fazli <120200477+AdamFazli@users.noreply.github.com>
2025-07-01 23:46:23 +0800

    fix vite file

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
+    base: '/', // Ensure base path is set to the root
     plugins: [
         laravel({
             input: [
