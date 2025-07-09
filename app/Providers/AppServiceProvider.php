228dd81861e58ec07de344afcac13e268f396d64
Adam Fazli <120200477+AdamFazli@users.noreply.github.com>
2025-07-10 03:40:11 +0800

    test storage

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\URL;
+use Illuminate\Support\Facades\File;
 use Illuminate\Support\Facades\Vite;
 
 class AppServiceProvider extends ServiceProvider
@@ -24,6 +25,12 @@ class AppServiceProvider extends ServiceProvider
         // Force HTTPS in production environment
         if (config('app.env') === 'production') {
             URL::forceScheme('https');
+            $storage = storage_path('app/public');
+        $public = public_path('storage');
+
+        if (!File::exists($public)) {
+            File::link($storage, $public);
+        }
         }
     }
 }
